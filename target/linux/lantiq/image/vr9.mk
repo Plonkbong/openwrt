41b48ebf5a9a5c930563150c5b2307ef76aa8013
Jonas Albrecht <plonkbong100@protonmail.com>
2020-10-10 01:08:47 +0000

    lantiq: xrx200 no default add of xdsl-firmware
    remove xdsl-firmware from default makefile and add them inside vr9.mk,
    this make it possible to build kernel-ramdidisk-images without these
    firmware.
    It needet to make them smaler (<5MB)
    
    Signed-off-by: Jonas Albrecht <plonkbong100@protonmail.com>

--- target/linux/lantiq/image/vr9.mk
+++ target/linux/lantiq/image/vr9.mk
@@ -6,6 +6,7 @@ define Device/alphanetworks_asl56026
   DEVICE_ALT0_VENDOR := BT Openreach
   DEVICE_ALT0_MODEL := ECI VDSL Modem V-2FUb/I
   IMAGE_SIZE := 7488k
+  DEVICE_PACKAGES := dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += alphanetworks_asl56026
 
@@ -18,7 +19,7 @@ define Device/arcadyan_arv7519rw22
   DEVICE_ALT1_VENDOR := Astoria Networks
   DEVICE_ALT1_MODEL := ARV7519RW22
   IMAGE_SIZE := 31232k
-  DEVICE_PACKAGES := kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += ARV7519RW22
 endef
 TARGET_DEVICES += arcadyan_arv7519rw22
@@ -28,6 +29,7 @@ define Device/arcadyan_vg3503j
   DEVICE_MODEL := ECI VDSL Modem V-2FUb/R
   IMAGE_SIZE := 8000k
   SUPPORTED_DEVICES += VG3503J
+  DEVICE_PACKAGES := dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += arcadyan_vg3503j
 
@@ -43,7 +45,7 @@ define Device/arcadyan_vgv7510kw22-brn
   SIGNATURE := BRNDA6431
   MAGIC := 0x12345678
   CRC32_POLY := 0x04c11db7
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += VGV7510KW22BRN
 endef
 TARGET_DEVICES += arcadyan_vgv7510kw22-brn
@@ -61,7 +63,7 @@ define Device/arcadyan_vgv7510kw22-nor-smp
 $(Device/arcadyan_vgv7510kw22-nor)
   DEVICE_VARIANT := NOR-SMP
   DEVICE_ALT0_VARIANT := NOR-SMP
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += arcadyan_vgv7510kw22-nor-smp
 
@@ -69,7 +71,7 @@ define Device/arcadyan_vgv7510kw22-nor-vpe
 $(Device/arcadyan_vgv7510kw22-nor)
   DEVICE_VARIANT := NOR-VPE
   DEVICE_ALT0_VARIANT := NOR-VPE
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += arcadyan_vgv7510kw22-nor-vpe
 
@@ -86,7 +88,7 @@ define Device/arcadyan_vgv7519-brn
   SIGNATURE := 5D00008000
   MAGIC := 0x12345678
   CRC32_POLY := 0x2083b8ed
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += VGV7519BRN
 endef
 TARGET_DEVICES += arcadyan_vgv7519-brn
@@ -99,7 +101,7 @@ define Device/arcadyan_vgv7519-nor
   DEVICE_ALT0_MODEL := Experiabox 8
   DEVICE_ALT0_VARIANT := NOR
   IMAGE_SIZE := 15360k
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += VGV7519NOR
 endef
 TARGET_DEVICES += arcadyan_vgv7519-nor
@@ -145,7 +147,7 @@ define Device/arcadyan_vgv953akw22-b-23-smp
   $(Device/arcadyan_vgv953akw22-b-23)
   DEVICE_VARIANT := SMP
   DEVICE_ALT0_VARIANT := SMP
-  DEVICE_PACKAGES := kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += arcadyan_vgv953akw22-b-23-smp
 
@@ -153,7 +155,7 @@ define Device/arcadyan_vgv953akw22-b-23-vpe
   $(Device/arcadyan_vgv953akw22-b-23)
   DEVICE_VARIANT := VPE
   DEVICE_ALT0_VARIANT := VPE
-  DEVICE_PACKAGES := kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc
+  DEVICE_PACKAGES := kmod-usb-dwc2 kmod-ltq-tapi kmod-ltq-vmmc dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += arcadyan_vgv953akw22-b-23-vpe
 
@@ -167,7 +169,7 @@ define Device/avm_fritz3370
   IMAGES += eva-kernel.bin eva-filesystem.bin
   IMAGE/eva-kernel.bin := append-kernel
   IMAGE/eva-filesystem.bin := append-ubi
-  DEVICE_PACKAGES := kmod-ath9k wpad-basic-wolfssl kmod-usb-dwc2 fritz-tffs
+  DEVICE_PACKAGES := kmod-ath9k wpad-basic-wolfssl kmod-usb-dwc2 fritz-tffs dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 
 define Device/avm_fritz3370-rev2-hynix
@@ -188,7 +190,7 @@ define Device/avm_fritz7360sl
   $(Device/AVM)
   DEVICE_MODEL := FRITZ!Box 7360 SL
   IMAGE_SIZE := 15744k
-  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += FRITZ7360SL
 endef
 TARGET_DEVICES += avm_fritz7360sl
@@ -198,7 +200,7 @@ define Device/avm_fritz7360-v2
   DEVICE_MODEL := FRITZ!Box 7360
   DEVICE_VARIANT := v2
   IMAGE_SIZE := 32128k
-  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += avm_fritz7360-v2
 
@@ -208,7 +210,7 @@ define Device/avm_fritz7362sl
   DEVICE_MODEL := FRITZ!Box 7362 SL
   KERNEL_SIZE := 4096k
   IMAGE_SIZE := 49152k
-  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 fritz-tffs
+  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 fritz-tffs dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += avm_fritz7362sl
 
@@ -219,7 +221,7 @@ define Device/avm_fritz7412
   BOARD_NAME := FRITZ7412
   KERNEL_SIZE := 4096k
   IMAGE_SIZE := 49152k
-  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl fritz-tffs-nand fritz-caldata
+  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl fritz-tffs-nand fritz-caldata dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += avm_fritz7412
 
@@ -230,7 +232,7 @@ define Device/bt_homehub-v5a
   DEVICE_VARIANT := Type A
   BOARD_NAME := BTHOMEHUBV5A
   DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader \
-	kmod-ath10k-ct ath10k-firmware-qca988x-ct wpad-basic-wolfssl kmod-usb-dwc2
+	kmod-ath10k-ct ath10k-firmware-qca988x-ct wpad-basic-wolfssl kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += BTHOMEHUBV5A
 endef
 TARGET_DEVICES += bt_homehub-v5a
@@ -239,7 +241,7 @@ define Device/buffalo_wbmr-300hpd
   DEVICE_VENDOR := Buffalo
   DEVICE_MODEL := WBMR-300HPD
   IMAGE_SIZE := 15616k
-  DEVICE_PACKAGES := kmod-mt7603 wpad-basic-wolfssl kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-mt7603 wpad-basic-wolfssl kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += WBMR300
 endef
 TARGET_DEVICES += buffalo_wbmr-300hpd
@@ -250,7 +252,7 @@ define Device/lantiq_easy80920-nand
   DEVICE_MODEL := VR9 EASY80920
   DEVICE_VARIANT := NAND
   IMAGE_SIZE := 64512k
-  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 kmod-usb-ledtrig-usbport dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += lantiq_easy80920-nand
 
@@ -259,7 +261,7 @@ define Device/lantiq_easy80920-nor
   DEVICE_MODEL := VR9 EASY80920
   DEVICE_VARIANT := NOR
   IMAGE_SIZE := 7936k
-  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-ath9k kmod-owl-loader wpad-basic-wolfssl kmod-usb-dwc2 kmod-usb-ledtrig-usbport dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += lantiq_easy80920-nor
 
@@ -275,6 +277,7 @@ define Device/netgear_dm200
   IMAGE_SIZE := 7872k
   NETGEAR_BOARD_ID := DM200
   NETGEAR_HW_ID := 29765233+8+0+64+0+0
+  DEVICE_PACKAGES := dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
 endef
 TARGET_DEVICES += netgear_dm200
 
@@ -284,7 +287,7 @@ define Device/zyxel_p-2812hnu-f1
   DEVICE_MODEL := P-2812HNU
   DEVICE_VARIANT := F1
   BOARD_NAME := P2812HNUF1
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-usb-ledtrig-usbport
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 kmod-usb-ledtrig-usbport dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   KERNEL_SIZE := 3072k
   SUPPORTED_DEVICES += P2812HNUF1
 endef
@@ -296,7 +299,7 @@ define Device/zyxel_p-2812hnu-f3
   DEVICE_MODEL := P-2812HNU
   DEVICE_VARIANT := F3
   BOARD_NAME := P2812HNUF3
-  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2
+  DEVICE_PACKAGES := kmod-rt2800-pci wpad-basic-wolfssl kmod-usb-dwc2 dsl-vrx200-firmware-xdsl-a dsl-vrx200-firmware-xdsl-b-patch
   SUPPORTED_DEVICES += P2812HNUF3
 endef
 TARGET_DEVICES += zyxel_p-2812hnu-f3
